c82141f9c9a9f893facefbf2a9c6c31acec388c7
JanPili <62252044+yayneek@users.noreply.github.com>
2025-09-24 18:33:43 +0200

    Added node to transfer lidar scan data to position of  the cube.

---
+++ Project2/launch/gazebo_display.launch.py
@@ -0,0 +1,198 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command, LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    package_share = get_package_share_directory('project2')
+
+
+    # === Gazebo ===
+    default_world = os.path.join(package_share, 'worlds', 'empty.world')
+
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description='World to load'
+    )
+
+    world = LaunchConfiguration(
+        'world'
+    )
+
+    gz_sim_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('ros_gz_sim'),
+                'launch',
+                'gz_sim.launch.py'
+            )
+        ), 
+        launch_arguments={
+            'gz_args': ['-r ',world]
+        }.items()
+    )
+
+    # Robot model spawn:
+    robot_model_sdf = os.path.join(package_share,'models','ur5_rg2','model.sdf')
+    robot_model_urdf = os.path.join(package_share,'models','ur5_rg2','model_minimal.urdf')
+    robot_spawn_node = Node(
+        package='ros_gz_sim',
+        executable='create',
+        name='spawn_robot',
+        output = 'screen',
+        arguments = [
+            '-name', 'ur5_rg2',
+            '-file', robot_model_sdf,
+            '-world', 'empty'
+        ]
+    )
+    # Spawning Lidars:
+    # Y:
+    lidar_y_urdf = os.path.join(package_share, 'sensors', 'lidar_y.sdf')
+    lidar_y_spawn_node = Node(
+        package='ros_gz_sim',
+        executable='create',
+        name='spawn_camera',
+        output = 'screen',
+        arguments=[
+            '-file', lidar_y_urdf,
+            '-name','lidar_y',
+            '-world','empty',
+            '-x','0.75',
+            '-y', '0.5',
+            '-z','0.1'
+        ]
+    )
+    static_transform_lidar_y_node = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=["0.75", "0.5", "0.1", "3.14159265359", "0", "0",  "Base", "lidar_y/lidar_link/lidar_sensor"],
+        name="static_transform_lidar_y",
+        output ="screen"
+    )
+    # X:
+    lidar_x_urdf = os.path.join(package_share, 'sensors', 'lidar_x.sdf')
+    lidar_x_spawn_node = Node(
+        package='ros_gz_sim',
+        executable='create',
+        name='spawn_camera',
+        output = 'screen',
+        arguments=[
+            '-file', lidar_x_urdf,
+            '-name','lidar_x',
+            '-world','empty',
+            '-x','0.0',
+            '-y','1.0',
+            '-z','0.1',
+        ]
+    )
+    static_transform_lidar_x_node = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=["0.0", "1", "0.1", "-1.57079632679", "0", "0",  "Base", "lidar_x/lidar_link/lidar_sensor"],
+        name='static_transform_lidar_x',
+        output ="screen"
+    )
+
+    # Cube model spawn:
+    cube_model_sdf = os.path.join(package_share,'models','other','cube.sdf')
+    cube_spawn_node = Node(
+        package='ros_gz_sim',
+        executable='create',
+        name='spawn_cube',
+        output ='screen',
+        arguments=[
+            '-name', 'cube',
+            '-file', cube_model_sdf,
+            '-x', '0.0',
+            '-y', '0.5',
+            '-z', '0.1',
+            '-world','empty'
+        ]
+    )
+
+    # Goal_plane spawn:
+    plane_model_sdf = os.path.join(package_share, 'models','other','goal_plane.sdf')
+    plane_spawn_node = Node(
+        package='ros_gz_sim',
+        executable='create',
+        name='spawn_plane',
+        output="screen",
+        arguments=[
+            '-name','goal_plane',
+            '-file',plane_model_sdf,
+            '-x', '0.0',
+            '-y','-0.5',
+            '-z','0.01',
+            '-world','empty'            
+        ]
+    )
+
+    # joint_state_publisher:
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+    )
+
+    # robot_state_publisher:
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        parameters=[
+            {'robot_description': open(robot_model_urdf).read()}
+        ],
+        output="screen"
+    )
+
+    # Rviz node:
+    rviz_config_path = os.path.join(package_share,'rviz','urdf.rviz')
+    rviz_arg = DeclareLaunchArgument(
+        name='rvizconfig',
+        default_value=str(rviz_config_path),
+        description='Path to rviz config file'
+    )
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', str(rviz_config_path)],
+    )
+
+    # gz to ros bridge:
+    bridge_params = os.path.join(package_share, 'config','gz_bridge.yaml')
+    ros_gz_bridge_node = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+            ]
+    )
+
+
+    return LaunchDescription([
+        joint_state_publisher_node,
+        world_arg,
+        rviz_arg,
+        gz_sim_launch,        
+        robot_spawn_node,
+        cube_spawn_node,
+        lidar_x_spawn_node,
+        lidar_y_spawn_node,
+        static_transform_lidar_x_node,
+        static_transform_lidar_y_node,
+        plane_spawn_node,
+        rviz_node,
+        robot_state_publisher_node,
+        ros_gz_bridge_node,
+    ])
---
+++ Project2/project2/lidar_to_pose.py
@@ -0,0 +1,116 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+from message_filters import Subscriber, ApproximateTimeSynchronizer
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Lidar_to_Pose(Node):
+    def __init__(self):
+        super().__init__('Lidar_to_Pose')
+
+        sub_x = Subscriber(self, LaserScan, 'scan_x')
+        sub_y = Subscriber(self, LaserScan, 'scan_y')
+
+        ats = ApproximateTimeSynchronizer([sub_x, sub_y], queue_size=10, slop=0.1)
+        ats.registerCallback(self.synced_callback)
+ 
+        plt.ion()
+        self.fig, self.ax = plt.subplots()
+
+    def synced_callback(self, scan_x: LaserScan, scan_y: LaserScan):
+        points_x = self.scan_to_points(scan_x, [0, 1, 0.1], math.pi*3/2)
+        points_y = self.scan_to_points(scan_y, [0.75, 0.5, 0.1], math.pi)
+        points = np.vstack([points_x, points_y])
+
+        obb = self.obb_pca(points)
+        center = obb['center']
+        self.get_logger().info(f"OBB center: {center}, width: {obb['width']:.2f}, height: {obb['height']:.2f}, angle: {obb['angle']:.2f} rad")
+
+
+        # czyszczenie wykresu i rysowanie nowych punktów
+        self.ax.clear()
+        self.ax.scatter(*zip(*points_x), s=2, c='r', label="Lidar X")
+        self.ax.scatter(*zip(*points_y), s=2, c='b', label="Lidar Y")
+        self.ax.legend()
+        self.ax.set_aspect('equal')
+        self.ax.set_title("Fuzja dwóch lidarów")
+        plt.draw()
+        plt.pause(0.01)  # odświeżenie wykresu
+    
+    def scan_to_points(self, scan: LaserScan, scanner_position, scanner_rotation):
+        def rot_mat(angle):
+            return np.array([
+                [np.cos(angle), -np.sin(angle)],
+                [np.sin(angle),  np.cos(angle)]
+            ])
+        points = []
+        angle = scan.angle_min
+        for r in scan.ranges:
+            if scan.range_min < r < scan.range_max:
+                local = np.array([r*math.cos(angle), r*math.sin(angle)])
+                global_pos = np.array(scanner_position[:2]) + np.dot(rot_mat(scanner_rotation), local)
+                points.append(global_pos)
+            angle += scan.angle_increment
+        return points
+
+    def obb_pca(self, points):
+        pts = np.asarray(points)
+        assert pts.ndim == 2 and pts.shape[1] == 2 and pts.shape[0] >= 2
+
+        # centroid i centrowanie
+        c = pts.mean(axis=0)
+        X = pts - c
+
+        # macierz kowariancji i PCA
+        cov = np.cov(X.T)         # 2x2
+        eigvals, eigvecs = np.linalg.eigh(cov)  # zapewnia rosnące wartości własne
+        # ustawiamy największy wektor własny jako pierwszą kolumnę
+        order = eigvals.argsort()[::-1]
+        eigvecs = eigvecs[:, order]
+        eigvals = eigvals[order]
+
+        # projekcja punktów na osie PCA
+        proj = X.dot(eigvecs)     # (N,2) - współrzędne w bazie PCA
+
+        # min/max w układzie PCA -> rozmiary pudełka
+        min_proj = proj.min(axis=0)
+        max_proj = proj.max(axis=0)
+        width = max_proj[0] - min_proj[0]
+        height = max_proj[1] - min_proj[1]
+
+        # rogi w układzie PCA (CCW)
+        corners_pca = np.array([
+            [min_proj[0], min_proj[1]],
+            [min_proj[0], max_proj[1]],
+            [max_proj[0], max_proj[1]],
+            [max_proj[0], min_proj[1]],
+        ])
+
+        # przekształć rogi z powrotem do układu globalnego
+        corners_global = corners_pca.dot(eigvecs.T) + c
+
+        # kąt orientacji (wektor własny 0 to oś główna)
+        main_axis = eigvecs[:, 0]
+        angle = math.atan2(main_axis[1], main_axis[0])  # radiany
+
+        return {
+            'center': c,
+            'width': width,
+            'height': height,
+            'angle': angle,
+            'corners': corners_global  # 4x2
+        }
+
+
+def main(args = None):
+    rclpy.init(args=args)
+    node = Lidar_to_Pose()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ =='__main__':
+    main()
